1367559b1e47d2a817cd868d1093cc805431d43b
286208466 <286208466@qq.com>
2023-03-22 10:09:47 +0800

    11

--- react_client_202303/src/App.tsx
+++ react_client_202303/src/App.tsx
@@ -1,25 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { Provider } from "react-redux";
+
+import { ConfigProvider } from "antd";
+import zhCN from 'antd/locale/zh_CN';
+import en_US from "antd/locale/en_US"
+import Cookies from "js-cookie";
+
+import "./assets/css/base.less";
+import "./assets/css/page.less";
+import "antd/dist/reset.css";
+
+import store from "./redux/store";
+
+// import NotFound from "./views/NotFound";
+// import Login from "./views/login";
+// import Demo from "./views/demo";
+// import DatavDemo1 from "./views/datav/demo1";
+// import Maptalks_3d from "./views/datav/maptalks_3d";
+// import Layout from "./layout/index";
+
+// import "./mock/index.js";
+
+// import dayjs from 'dayjs';
+// import 'dayjs/locale/zh-cn';
+// dayjs.locale('zh-cn');
+
+import moment from "moment";
+import "moment/locale/zh-cn";
+moment.locale("zh-cn");
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ConfigProvider
+      locale={zhCN}
+      theme={{
+        token: {
+          colorPrimary: "#00b96b",
+        },
+      }}
+    >
+      <div className="App"></div>
+    </ConfigProvider>
   );
 }
 
